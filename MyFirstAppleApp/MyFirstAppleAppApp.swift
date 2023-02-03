8ec93e68bb1fbf5229818fd0c14be7355711db6e
jacksonhuang1229 <gamegamelife@gmail.com>
2023-02-03 12:14:29 -0500

    Initial Commit

---
+++ MyFirstAppleApp/MyFirstAppleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyFirstAppleAppApp.swift
+//  MyFirstAppleApp
+//
+//  Created by Jackson Huang on 2023/2/3.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstAppleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
